dd664a2bf2494250fc32674b07007043d7ac10ff
clairelyles <claire.lyles@gmail.com>
2015-01-19 21:16:52 -0800

    Added jQuery toggle effect to Step 1 Grooming

--- js/script.js
+++ js/script.js
@@ -13,4 +13,23 @@ $(function() {
       $(this).show();
     });
 
+    $('#grooming_icon1').hover(function(){
+      $(this).toggle({ effect: 'scale', percent: 150 })
+    }, function(){
+      $(this).fadeIn(1000,1)
+    })
+
+    $('#grooming_icon2').hover(function(){
+      $(this).toggle({ effect: 'scale', percent: 150 })
+    }, function(){
+      $(this).fadeIn(1000)
+    })
+
+    $('#grooming_icon3').hover(function(){
+      $(this).toggle({ effect: 'scale', percent: 150 })
+    }, function(){
+      $(this).fadeIn(1000);
+      return false;
+    })
+
 });
